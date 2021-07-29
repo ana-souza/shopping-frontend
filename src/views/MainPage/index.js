37880392a52959b134d2b0ced8839494c0032018
LucasPCBranco <lucas.pcbranco@hotmail.com>
2021-07-28 22:06:43 -0300

    changing consumerUri refs

--- src/views/MainPage/index.js
+++ src/views/MainPage/index.js
@@ -243,14 +243,14 @@ class MainPage extends Component {
 
     getRecommendations = async () => {
 
-        var raw = JSON.stringify({"consumerUri":this.state.consumerUri});
+        var raw = JSON.stringify({"consumerUri":this.state.username});
 
         var requestOptions = {
             method: 'GET',
             redirect: 'follow'
           };
     
-          await fetch(`http://localhost:8080/product/recommendation?consumerUri=${encodeURIComponent(this.state.consumerUri)}`, requestOptions)
+          await fetch(`http://localhost:8080/product/recommendation?consumerUri=${this.state.username}`, requestOptions)
             .then(response => response.json())
             .then(result => 
                 this.setState(
